26deac059e31d8bb72e71a588b1b83937df55c46
chanoktrue <chanoktrue@me.com>
2024-07-25 22:07:16 +0700

    swiftdata basic

--- SwiftDataDemo2/ContentView.swift
+++ SwiftDataDemo2/ContentView.swift
@@ -6,16 +6,52 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
+    @Environment(\.modelContext) var context
+    @Query private var products: [Product]
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Tap on this button to add data")
+            Button("Add") {
+                addProdcut()
+            }
+            List {
+                ForEach(products) { product in
+                    HStack {
+                        Text(product.name)
+                        Spacer()
+                        Button(action: {
+                            updateProduct(product: product)
+                        }, label: {
+                            Text("Button")
+                        })
+                    }
+                }
+                .onDelete(perform: { indexSet in
+                    for index in indexSet {
+                        deleteProduct(product: products[index])
+                    }
+                })
+            }
         }
         .padding()
+    }
+    
+    private func addProdcut() {
+        let p = Product(name: "Test Add")
+        context.insert(p)
+    }
+    
+    private func deleteProduct(product: Product) {
+        context.delete(product)
+    }
+    
+    private func updateProduct(product: Product) {
+        product.name = "update"
+        try? context.save()
     }
 }
 
--- SwiftDataDemo2/Product.swift
+++ SwiftDataDemo2/Product.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+import SwiftData
+
+@Model
+class Product: Identifiable {
+    let id: String
+    var name: String
+    
+    init(name: String) {
+        self.id = UUID().uuidString
+        self.name = name
+    }
+    
+}
