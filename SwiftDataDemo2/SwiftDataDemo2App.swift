cf7b3b5d342dd58dbe39bd44a4f0d2d707ea01b7
chanoktrue <chanoktrue@me.com>
2024-07-25 21:39:31 +0700

    Initial Commit

---
+++ SwiftDataDemo2/SwiftDataDemo2App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataDemo2App.swift
+//  SwiftDataDemo2
+//
+//  Created by Thongchai Subsaidee on 25/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataDemo2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
